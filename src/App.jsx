b1479c22f424120fb81cc8bcb20335c7059d2014
Amar Gill <amardeep@clearbanc.com>
2021-12-27 22:03:27 -0500

    basic Particle component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
+import Particle from "./components/Particle";
 
 export default function App() {
   return (
     <div style={{width: '100%', height: '100%'}} id="#canvas-container">
-      <Canvas>
+      <Canvas orthographic>
         <OrbitControls enablePan={true} enableZoom={true} enableRotate={true} />
         <ambientLight intensity={0.1} />
-        <directionalLight color="pink" position={[0, 0, 5]} />
-        <mesh>
+        <directionalLight color="green" position={[0, 5, 5]} />
+        <mesh position={[-10, 0, 0]}>
           <boxGeometry args={[2, 2, 2]} />
           <meshStandardMaterial />
         </mesh>
+        <Particle showRing showArrow/>
+        <gridHelper args={[20,40]} />
       </Canvas>
     </div>
   );
