0aa4d2570d1f8636cf4a3bfd87837559b5872c44
Amar Gill <amardeep@clearbanc.com>
2021-12-28 17:46:26 -0500

    updated background color

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ export default function App() {
   return (
     <div style={{ width: '100%', height: '100%' }} id="#canvas-container">
       <Canvas orthographic>
+        <color attach="background" args={['#0e1a2d']} />
         <OrbitControls enablePan={true} enableZoom={true} enableRotate={true} />
         <ambientLight intensity={0.1} color={'white'} />
         <directionalLight color="white" position={[0, 5, 5]} />
