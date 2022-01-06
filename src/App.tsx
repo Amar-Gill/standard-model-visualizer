39db85da13ce9639578d511e20f41df399a692ba
Amar Gill <amardeep@clearbanc.com>
2022-01-05 23:48:53 -0500

    set up basic page content that overlaps canvas. + scrollable

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,29 @@
-import { OrbitControls } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 
+import PageContent from '@/components/PageContent';
 import QuantumParticle from '@/components/QuantumParticle';
 
 export default function App() {
   return (
-    <div style={{ width: '100%', height: '100%' }} id="#canvas-container">
-      <Canvas orthographic>
-        <color attach="background" args={['#0e1a2d']} />
-        <OrbitControls enablePan={true} enableZoom={true} enableRotate={true} />
-        <ambientLight intensity={0.1} color={'white'} />
-        <directionalLight color="white" position={[0, 5, 5]} />
-        <QuantumParticle showRing showArrow />
-        <gridHelper args={[20, 40]} />
-      </Canvas>
-    </div>
+    <>
+      <div
+        style={{
+          width: '100%',
+          height: '100%',
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          zIndex: 1,
+        }}>
+        <Canvas orthographic>
+          <color attach="background" args={['#0e1a2d']} />
+          <ambientLight intensity={0.1} color="white" />
+          <directionalLight color="white" position={[0, 5, 5]} />
+          <QuantumParticle showRing showArrow />
+          <gridHelper args={[20, 40]} />
+        </Canvas>
+      </div>
+      <PageContent />
+    </>
   );
 }
---
+++ src/components/PageContent.tsx
@@ -0,0 +1,94 @@
+export default function PageContent() {
+  return (
+    <div
+      style={{
+        zIndex: 10,
+        color: 'white',
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        height: '100%',
+        width: '100%',
+        overflowY: 'scroll',
+      }}>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita nihil
+        praesentium, autem aut obcaecati quae fuga facilis ex minus, temporibus incidunt
+        sunt ipsum enim at tempora nesciunt facere quaerat, laboriosam quia iste hic
+        reiciendis quis odio? Laboriosam id non distinctio.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita nihil
+        praesentium, autem aut obcaecati quae fuga facilis ex minus, temporibus incidunt
+        sunt ipsum enim at tempora nesciunt facere quaerat, laboriosam quia iste hic
+        reiciendis quis odio? Laboriosam id non distinctio.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita nihil
+        praesentium, autem aut obcaecati quae fuga facilis ex minus, temporibus incidunt
+        sunt ipsum enim at tempora nesciunt facere quaerat, laboriosam quia iste hic
+        reiciendis quis odio? Laboriosam id non distinctio.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita nihil
+        praesentium, autem aut obcaecati quae fuga facilis ex minus, temporibus incidunt
+        sunt ipsum enim at tempora nesciunt facere quaerat, laboriosam quia iste hic
+        reiciendis quis odio? Laboriosam id non distinctio.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita nihil
+        praesentium, autem aut obcaecati quae fuga facilis ex minus, temporibus incidunt
+        sunt ipsum enim at tempora nesciunt facere quaerat, laboriosam quia iste hic
+        reiciendis quis odio? Laboriosam id non distinctio.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita nihil
+        praesentium, autem aut obcaecati quae fuga facilis ex minus, temporibus incidunt
+        sunt ipsum enim at tempora nesciunt facere quaerat, laboriosam quia iste hic
+        reiciendis quis odio? Laboriosam id non distinctio.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita nihil
+        praesentium, autem aut obcaecati quae fuga facilis ex minus, temporibus incidunt
+        sunt ipsum enim at tempora nesciunt facere quaerat, laboriosam quia iste hic
+        reiciendis quis odio? Laboriosam id non distinctio.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita nihil
+        praesentium, autem aut obcaecati quae fuga facilis ex minus, temporibus incidunt
+        sunt ipsum enim at tempora nesciunt facere quaerat, laboriosam quia iste hic
+        reiciendis quis odio? Laboriosam id non distinctio.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita nihil
+        praesentium, autem aut obcaecati quae fuga facilis ex minus, temporibus incidunt
+        sunt ipsum enim at tempora nesciunt facere quaerat, laboriosam quia iste hic
+        reiciendis quis odio? Laboriosam id non distinctio.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita nihil
+        praesentium, autem aut obcaecati quae fuga facilis ex minus, temporibus incidunt
+        sunt ipsum enim at tempora nesciunt facere quaerat, laboriosam quia iste hic
+        reiciendis quis odio? Laboriosam id non distinctio.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita nihil
+        praesentium, autem aut obcaecati quae fuga facilis ex minus, temporibus incidunt
+        sunt ipsum enim at tempora nesciunt facere quaerat, laboriosam quia iste hic
+        reiciendis quis odio? Laboriosam id non distinctio.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita nihil
+        praesentium, autem aut obcaecati quae fuga facilis ex minus, temporibus incidunt
+        sunt ipsum enim at tempora nesciunt facere quaerat, laboriosam quia iste hic
+        reiciendis quis odio? Laboriosam id non distinctio.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita nihil
+        praesentium, autem aut obcaecati quae fuga facilis ex minus, temporibus incidunt
+        sunt ipsum enim at tempora nesciunt facere quaerat, laboriosam quia iste hic
+        reiciendis quis odio? Laboriosam id non distinctio.
+      </p>
+    </div>
+  );
+}
