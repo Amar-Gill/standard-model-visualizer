b4370f19475e58036ab30687df8a5fb10ade70f8
Amar Gill <amardeep@clearbanc.com>
2022-01-15 16:47:46 -0500

    can move mesh collection group position based on scrolled offset value

--- src/components/MeshCollection.tsx
+++ src/components/MeshCollection.tsx
@@ -1,8 +1,18 @@
+import { useScroll } from '@react-three/drei';
+import { useFrame } from '@react-three/fiber';
+import { useRef } from 'react';
+import { Mesh } from 'three';
+
 import QuantumParticle from '@/components/QuantumParticle';
 
 export default function MeshCollection(props: JSX.IntrinsicElements['group']) {
+  const ref = useRef<Mesh>(null!);
+  const scroll = useScroll();
+
+  useFrame(() => (ref.current.position.z = scroll.offset * -10));
+
   return (
-    <group {...props}>
+    <group ref={ref} {...props}>
       <QuantumParticle showRing showArrow position={[0, 0, -8]} />
       <QuantumParticle showRing showArrow position={[0, 0, 0]} />
       <QuantumParticle showRing showArrow position={[0, 0, 8]} />
