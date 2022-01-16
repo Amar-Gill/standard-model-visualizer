4098e62b032a0534d703f718a77112d5be8fd0dd
Amar Gill <amargill@Amars-MacBook-Air.local>
2022-01-16 11:53:32 -0500

    WIP: writing first draft of webpage

--- src/components/PageContent.tsx
+++ src/components/PageContent.tsx
@@ -8,16 +8,17 @@ export default function PageContent() {
       }}>
       <h1>Introduction</h1>
       <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita nihil
-        praesentium, autem aut obcaecati quae fuga facilis ex minus, temporibus incidunt
-        sunt ipsum enim at tempora nesciunt facere quaerat, laboriosam quia iste hic
-        reiciendis quis odio? Laboriosam id non distinctio.
+        All matter in the universe is made up of atoms and molecules. Atoms are composed
+        of protons, neutrons and electrons. But is there anything smaller than that?
+        Beyond the sub-atomic particles that compose atoms is the world of quantum
+        mechanics. The theory that describes these quantum particles is known as the
+        Standard Model of Particle Physics.
       </p>
       <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita nihil
-        praesentium, autem aut obcaecati quae fuga facilis ex minus, temporibus incidunt
-        sunt ipsum enim at tempora nesciunt facere quaerat, laboriosam quia iste hic
-        reiciendis quis odio? Laboriosam id non distinctio.
+        The Standard Model has been developed over years. It is not yet complete, but it
+        is our most accurate description of reality at the quantum scale. It successfully
+        models most of the quantum mechanical equations that govern our physical reality.
+        Below is a picture of the most common representation of the Standard Model.
       </p>
       <img
         src="/src/standard-model.svg"
@@ -25,42 +26,34 @@ export default function PageContent() {
         style={{ width: 'fit-content' }}
       />
       <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita nihil
-        praesentium, autem aut obcaecati quae fuga facilis ex minus, temporibus incidunt
-        sunt ipsum enim at tempora nesciunt facere quaerat, laboriosam quia iste hic
-        reiciendis quis odio? Laboriosam id non distinctio.
+        The above table is a good start. But it fails to communicate the quantum
+        interactions that occur between these particles. And some key quantum properties
+        are simply missing.
       </p>
       <h2>Quarks</h2>
       <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita nihil
-        praesentium, autem aut obcaecati quae fuga facilis ex minus, temporibus incidunt
-        sunt ipsum enim at tempora nesciunt facere quaerat, laboriosam quia iste hic
-        reiciendis quis odio? Laboriosam id non distinctio.
+        Matter is comprised of two main types of particles. Quarks and Leptons. The two
+        types of quarks that comprise protons and neutrons are the up quark and the down
+        quark. Up quarks have 2/3 of a unit of electric charge and the down quark has 1/3
+        units.
       </p>
       <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita nihil
-        praesentium, autem aut obcaecati quae fuga facilis ex minus, temporibus incidunt
-        sunt ipsum enim at tempora nesciunt facere quaerat, laboriosam quia iste hic
-        reiciendis quis odio? Laboriosam id non distinctio.
+        There is also a quantum property known as spin, which refers to the direction the
+        particle rotates with respect to it's direction of travel. "Left handed" quarks
+        spin clockwise and "Right handed" quarks spin counter clockwise.
       </p>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita nihil
-        praesentium, autem aut obcaecati quae fuga facilis ex minus, temporibus incidunt
-        sunt ipsum enim at tempora nesciunt facere quaerat, laboriosam quia iste hic
-        reiciendis quis odio? Laboriosam id non distinctio.
-      </p>
+      <p>TODO</p>
       <h2>Weak Force</h2>
       <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita nihil
-        praesentium, autem aut obcaecati quae fuga facilis ex minus, temporibus incidunt
-        sunt ipsum enim at tempora nesciunt facere quaerat, laboriosam quia iste hic
-        reiciendis quis odio? Laboriosam id non distinctio.
+        The first quantum interaction we will explore is the Weak Interaction. It refers
+        to the Weak Nuclear force acting between two quarks. One of the force carrier for
+        the weak force is the W Boson. It has an electric charge of +1 or -1.
       </p>
       <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita nihil
-        praesentium, autem aut obcaecati quae fuga facilis ex minus, temporibus incidunt
-        sunt ipsum enim at tempora nesciunt facere quaerat, laboriosam quia iste hic
-        reiciendis quis odio? Laboriosam id non distinctio.
+        A consequence of the weak interaction is that a left handed up and down quarks can
+        transform into each when they exchange a W boson. No right handed W bosons have
+        been observed in nature, so right handed quarks do not emit or absorb W bosons,
+        and cannot transform into each other.
       </p>
       <p>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita nihil
@@ -70,23 +63,21 @@ export default function PageContent() {
       </p>
       <h2>Strong Force and Color</h2>
       <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita nihil
-        praesentium, autem aut obcaecati quae fuga facilis ex minus, temporibus incidunt
-        sunt ipsum enim at tempora nesciunt facere quaerat, laboriosam quia iste hic
-        reiciendis quis odio? Laboriosam id non distinctio.
+        Quarks have another quantum property known as color. The value can be red, blue or
+        green. The color charge makes it sensitive to the strong nuclear force. It is the
+        strong force that binds quarks of different color together into composite
+        particles such as protons and neutrons, which end up with no net color charge, and
+        are considered "colorless".
       </p>
       <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita nihil
-        praesentium, autem aut obcaecati quae fuga facilis ex minus, temporibus incidunt
-        sunt ipsum enim at tempora nesciunt facere quaerat, laboriosam quia iste hic
-        reiciendis quis odio? Laboriosam id non distinctio.
+        The force carrier of the strong force is the gluon. Much like with W bosons,
+        quarks of different color charge transform into one another by exchanging gluons.
+        Gluons themselves have color charge, and therefore interact with each other as
+        well. The interaction of gluons between quarks and amongst themselves can be
+        modeled as a solid triangle between quarks. Both right handed and left handed
+        quarks partake in the strong interaction.
       </p>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita nihil
-        praesentium, autem aut obcaecati quae fuga facilis ex minus, temporibus incidunt
-        sunt ipsum enim at tempora nesciunt facere quaerat, laboriosam quia iste hic
-        reiciendis quis odio? Laboriosam id non distinctio.
-      </p>
+      <p>TODO</p>
       <h2>Leptons</h2>
       <p>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita nihil
